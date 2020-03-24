2df2284beea74889390a7134224be0875ca3bf7f
Brandon Lafving <brandon@lafving.com>
2020-03-24 00:06:49 -0400

    model functions to calculate daily calorie balance

--- sandbox/mydata/models.py
+++ sandbox/mydata/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     lastname = models.CharField('Last Name', max_length=50)
     height = models.SmallIntegerField('Height (in.)', default=64)
     weight = models.SmallIntegerField('Weight (lbs.)', default=150)
+    age = models.SmallIntegerField('Age', default=38)
     GENDERS = [
         ('ML', 'Male'),
         ('FM', 'Female'),
@@ -22,6 +23,11 @@ class User(models.Model):
 
     def __str__(self):
         return self.firstname + ' ' + self.lastname
+
+    def base_metabolic_rate(self):
+        if self.gender == 'ML':
+            return 88.362 + (13.397 * self.weight / 2.205) \
+                + (4.799 * self.height * 2.54) - (5.677 * self.age)        
 
 class Nutrient(models.Model):
     date = models.DateField(primary_key=True)
@@ -51,6 +57,9 @@ class Nutrient(models.Model):
         self.protein = api_day.totals['protein']
         self.save()
         return str(self.date) + 'nutrition updated'
+    
+    def cal_balance(self):
+        return self.calories - self.user_rec.base_metabolic_rate()
 
 
 """
